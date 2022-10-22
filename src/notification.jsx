4d96e1fcc37a220a024f80a2d01407a52c8a98b3
Pradyumna-Hegde <pradyumna.coder@gmail.com>
2022-10-22 20:28:08 +0530

    modified the notification.jsx file and added the code to build notification design

--- src/notification.jsx
+++ src/notification.jsx
@@ -1,7 +1,13 @@
 const Notification = () => {
   return (
-    <div>
-      <img src="logo512.png" alt="react notification" />
+    <div className="p-6 max-w-sm bg-white rounded-lg shadow-lg flex items-center space-x-4">
+      <div className="shrink-0">
+        <img className="h-12 w-12" src="logo512.png" alt="React  logo" />
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-slate-500">You have a new message!</p>
+      </div>
     </div>
   );
 };
